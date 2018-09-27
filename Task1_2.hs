64918fac97110cfffa3d5667bb067c838adcb066
gilgenbergg <lanasvega@gmail.com>
2018-09-27 03:00:12 +0300

    First _backup_ with bugs, incorrect syntax and allignment for Task_1_2

--- Task1_2.hs
+++ Task1_2.hs
@@ -12,7 +12,11 @@ cos x = todo
 
 -- наибольший общий делитель двух чисел
 gcd :: Integer -> Integer -> Integer
-gcd x y = todo
+gcd x y = let
+	if x == 0 then = y
+	if y == 0 then = x
+	else if x>y = gcd y (x `mod` y)
+		else gcd x (y `mod` x)
 
 -- существует ли полный целочисленный квадрат в диапазоне [from, to)?
 doesSquareBetweenExist :: Integer -> Integer -> Bool
@@ -26,11 +30,29 @@ isDateCorrect day month year = todo
 -- возведение числа в степень, duh
 -- готовые функции и плавающую арифметику использовать нельзя
 pow :: Integer -> Integer -> Integer
-pow x y = todo
+pow x y = let
+	if y == 0 = 1
+	if even y then = pow(x*x) (y `div` 2)
+	else = (pow (x*x) ((y-1) `div` 2) * x
 
 -- является ли данное число простым?
 isPrime :: Integer -> Bool
-isPrime x = todo
+	isPrime x = if x<=1 
+		then False
+		else bodyIsPrime
+	where bodyIsPrime = let
+		dividers = [2..x]
+		checked = []
+		recursionCheck = let
+			if x `mod` (last dividers) <> 0
+				then init dividers
+					recursionCheck
+				else checked ++ [last dividers]
+					init dividers
+						recursionCheck
+			if null checked then True
+			else False			
+	
 
 type Point2D = (Double, Double)
 
