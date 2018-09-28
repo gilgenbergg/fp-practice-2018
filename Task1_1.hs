668798ea3ca4a09e96979c642fa738d22fa80278
gilgenbergg <lanasvega@gmail.com>
2018-09-29 01:23:42 +0300

    2 of 3 points in Task1_1 were implemented, first one fixed, second in testing

--- Task1_1.hs
+++ Task1_1.hs
@@ -7,24 +7,32 @@ data BinaryOperation = Plus | Minus | Multiplex deriving (Show, Eq)
 
 data Term = IntConstant{ intValue :: Int }           -- числовая константа
             | Variable{ varName :: String }          -- переменная
-            | BinaryTerm{ lhv :: Term, rhv :: Term } -- бинарная операция
+            | BinaryTerm{ lhv :: Term, rhv :: Term, binop :: BinaryOperation } -- бинарная операция
             deriving(Show,Eq)
 
 -- Для бинарных операций необходима не только реализация, но и адекватные
 -- ассоциативность и приоритет
 (|+|) :: Term -> Term -> Term
 (|+|) l r = BinaryTerm l r Plus
-infixl 6 |+| Term -> Term
+infixl 6 |+| 
+
+(|-|) :: Term -> Term -> Term
 (|-|) l r = BinaryTerm l r Minus
-infixl 6 |-| Term -> Term
+infixl 6 |-| 
+
 (|*|) :: Term -> Term -> Term
-(|*|) l r BinaryTerm l r Multiplex
-infixl 7 |*| Term -> Term
+(|*|) l r = BinaryTerm l r Multiplex
+infixl 7 |*| 
 
 -- Заменить переменную `varName` на `replacement`
 -- во всём выражении `expression`
+
 replaceVar :: String -> Term -> Term -> Term
-replaceVar varName replacement expression = todo
+replaceVar varName replacement expression = 
+		case expression of 
+			(Variable var) -> if var == varName then replacement else expression
+			(BinaryTerm lhv rhv binop) -> BinaryTerm (replaceVar varName replacement lhv) (replaceVar varName replacement rhv) binop 
+			_ -> expression
 
 -- Посчитать значение выражения `Term`
 -- если оно состоит только из констант
