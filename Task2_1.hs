2ce758f1c70f789684ac979af60d52560dc344f1
gilgenbergg <lanasvega@gmail.com>
2018-10-25 01:22:16 +0300

    Second task is completed

--- Task2_1.hs
+++ Task2_1.hs
@@ -4,10 +4,10 @@ import Todo(todo)
 import Prelude hiding (lookup)
 -- Ассоциативный массив на основе бинарного дерева поиска
 -- Ключи - Integer, значения - произвольного типа
-data TreeMap v =   EmptyTree
-				  |Leaf Integer v
-				  |Node Integer v (TreeMap v) (TreeMap v)
-				  deriving(Show, Eq)
+data TreeMap v =    EmptyTree
+                  | Leaf Integer v
+                  | Node Integer v (TreeMap v) (TreeMap v)
+                  deriving(Show, Eq)
 
 -- Пустое дерево
 emptyTree :: TreeMap v
@@ -16,55 +16,85 @@ emptyTree =  EmptyTree
 -- Содержится ли заданный ключ в дереве?
 contains :: TreeMap v -> Integer -> Bool
 contains EmptyTree _         = False
-contains (Leaf k v) e	     | (k == e) = True
-					         | otherwise = False
+contains (Leaf k v) e        | (k == e) = True
+                             | otherwise = False
 contains (Node k v l r) e    | (k == e) = True
                              | (k < e) = contains r e
                              | (k > e) = contains l e
 
 -- Значение для заданного ключа
 lookup :: Integer -> TreeMap v -> v
-lookup _ EmptyTree 			= error "Search in empty tree"
-lookup e (Leaf k v)  		| (e == k) = v
-lookup e (Node k _ _ r)		| (e < k) = lookup k r
-lookup e (Node k _ l _)		| (e > k) = lookup k l
+lookup _ EmptyTree          = error "Emptyness inside an empty tree :("
+lookup e (Leaf k v)         | (e == k) = v
+lookup e (Node k v l r)     | (e < k) = lookup k r
+                            | (e > k) = lookup k l
+                            | otherwise = v
 
 -- Вставка пары (ключ, значение) в дерево
 insert :: (Integer, v) -> TreeMap v -> TreeMap v
 insert (k, v) EmptyTree = Leaf k v
-insert (k, v) (Node key value left right)  | (k == key) = error "Key is already taken"
-								           | (k < key) = Node key value (insert (k, v) left) right
-								           | (k > key) = Node key value left (insert (k,v) right)
-
-
-
--- Построение списка пар из дерева
---listFromTree :: TreeMap v -> [(Integer, v)]
---listFromTree EmptyTree = []
---listFromTree Leaf key value = [(key, value)]
---listFromTree Node key value left right = listFromTree left ++ [(key, value)] ++ listFromTree right
+insert (k, v) (Leaf key value)   | (k == key) = error "Key is already taken, you are late :("
+                                 | (k < key) = Node key value (Leaf k v) EmptyTree
+                                 | (k > key) = Node key value EmptyTree (Leaf k v)
+insert (k, v) (Node key value left right)  | (k == key) = error "Key is already taken, you are late :("
+                                           | (k < key) = Node key value (insert (k, v) left) right
+                                           | (k > key) = Node key value left (insert (k,v) right)
 
 
 -- Построение дерева из списка пар
---treeFromList :: [(Integer, v)] -> TreeMap v
---treeFromList lst = foldr (insert) EmptyTree lst
+treeFromList :: [(Integer, v)] -> TreeMap v
+treeFromList lst = foldr (insert) EmptyTree lst
+
+-- Построение списка пар из дерева
+listFromTree :: TreeMap v -> [(Integer, v)]
+listFromTree EmptyTree = []
+listFromTree (Leaf key value) = [(key, value)]
+listFromTree (Node key value left right) = listFromTree left ++ [(key, value)] ++ listFromTree right
 
 
 -- Удаление элемента по ключу, при удалении узла с поддеревьями, вместо узла встает 
 -- первое поддерево, а второе становится поддерревом первого
---remove :: Integer -> TreeMap v -> TreeMap v
---remove k EmptyTree = EmptyTree
---remove k Leaf key value 			 | (k == key) = EmptyTree
---						  			 | otherwise = Leaf key value
---remove k Node key value left right   | (k == key) = treeFromList (listFromTree left) ++ (listFromTree right)
---									 | (k > key) = --Node key value (remove k left) right-- TODO
---									 | (k < key) = --Node key value left (remove k right)-- TODO
+remove :: Integer -> TreeMap v -> TreeMap v
+remove k EmptyTree = EmptyTree
+remove k (Leaf key value)            | (k == key) = EmptyTree
+                                     | otherwise = Leaf key value
+remove k (Node key value left right) | (k == key) = treeFromList ((listFromTree right) ++ (listFromTree left))
+                                     | (k < key) = Node key value (remove k left) right 
+                                     | (k > key) = Node key value left (remove k right)
 
 
 -- Поиск ближайшего снизу ключа относительно заданного
 nearestLE :: Integer -> TreeMap v -> (Integer, v)
-nearestLE i t = todo
+nearestLE _ EmptyTree                                  = error "Emptyness inside an empty tree :("
+nearestLE k (Leaf key value)                           = error "Very very lonely leaf :("
+nearestLE k (Node key value left right)                | (key == k) = (key, value)
+                                                       | (k < key) = checkLeft k left
+ where 
+   checkLeft k (Node key value left (Node i val ll rr))  | (k == i) = (i, val)
+                                                         | (k /= i) = nearestLE i (Node i val ll rr)                                                             
+                                                         | otherwise = (key, value)
 
--- Поиск k-той порядковой статистики дерева 
+--Вычисление размера дерева
+size :: TreeMap v -> Integer
+size EmptyTree = 0
+size (Leaf key value) = 1
+size (Node key value left right) = size left + 1 + size right
+
+-- Поиск k-той порядковой статистики дерева
 kMean :: Integer -> TreeMap v -> (Integer, v)
-kMean i t = todo
+kMean _ EmptyTree = error "Emptyness inside an empty tree :("
+kMean k (Leaf key value)                | (k == 0) = (key, value)
+                                        | otherwise = error "Just a looonely leaf :("
+kMean k (Node key value left right)     | (k < size left) = kMean k left
+                                        | (k > size left) = kMean (k - size left - 1) right
+                                        | otherwise = (key, value)
+
+-- Поиск k-той порядковой статистики дерева (нерекурсивный способ)
+--kMean :: Integer -> TreeMap v -> (Integer, v)
+--kMean _ EmptyTree = error "Tree is empty"
+--kMean k t = kMean' (fromIntegral k) t 
+  --where
+    --kMean' k t | k >= l = error "Error"
+      --         | k < 0 = error "Error"
+    --           | k < l = listFromTree(t) !! k 
+  --  l = length(listFromTree(t))
--- Task2_2.hs
+++ Task2_2.hs
@@ -7,16 +7,16 @@ import Prelude hiding (foldl, foldr, unfoldr, map, concatMap,
 
 foldl :: (b -> a -> b) -> b -> [a] -> b
 foldl f acc [] = acc
-foldl f acc (h:t) = foldl f (f acc h) t
+foldl f acc (h : t) = foldl f (f acc h) t
 
 foldr :: (a -> b -> b) -> b -> [a] -> b
 foldr f acc [] = acc 
-foldr f acc (h:t) = f h (foldr f acc t)
+foldr f acc (h : t) = f h (foldr f acc t)
 
 unfoldr :: (b -> Maybe (a, b)) -> b -> [a]
 unfoldr f b = case f b of
     Nothing -> []
-    Just (a, b') -> a: unfoldr f b'
+    Just (a, b') -> a : unfoldr f b'
 
 -- Сумма всех элементов списка (пример)
 sum :: [Integer] -> Integer
@@ -24,30 +24,32 @@ sum lst = foldl (+) 0 lst
 
 -- Переворот списка (Пример)
 reverse :: [a] -> [a]
-reverse lst = foldl f [] lst where f t h = h:t
+reverse lst = foldl f [] lst where f t h = h : t
 
 -- Отображение элементов списка
 map :: (a -> b) -> [a] -> [b]
-map f list= foldr (\x acc -> (f x) : acc) [] list
+map f list = foldr (\x acc -> (f x) : acc) [] list
 
 -- Произведение всех элементов списка
 product :: [Integer] -> Integer
 product lst = foldr (*) 1 lst
 
-
 -- Выделение из списка Maybe всех существующих значений
 catMaybes :: [Maybe a] -> [a]
 catMaybes = foldr (\x acc -> case x of 
-    (Just a) -> a: acc 
+    (Just a) -> a : acc 
     Nothing -> acc) []
+
 
 -- Диагональ матрицы
 diagonal :: [[a]] -> [a]
-diagonal = todo
+diagonal = reverse . fst . foldl (\(r,c) s -> ((s !! c) : r, c+1)) ([], 0)
+
 
 -- Фильтр для всех элементов, не соответствующих предикату
 filterNot :: (a -> Bool) -> [a] -> [a]
-filterNot = todo
+filterNot filter lst =foldr (\x acc -> if (filter x) then acc else x : acc) [] lst
+
 
 -- Поиск элемента в списке
 elem :: (Eq a) => a -> [a] -> Bool
@@ -62,9 +64,17 @@ rangeTo from to step = foldl(\ acc x ->
 
 -- Конкатенация двух списков
 append :: [a] -> [a] -> [a]
-append first second = foldr(\x acc -> x: acc) second first
+append first second = foldr(\x acc -> x : acc) second first
+
+--Разбиение на n кусков
+mySplitAt :: Integer -> [a] -> ([a], [a])
+mySplitAt n = foldr (\x (xs, xz) -> if fst x <= n then (snd x : xs, xz) 
+                                                        else (xs, snd x : xz)) 
+                                                        ([], []) 
+                                                        . zip[1..]
 
 -- Разбиение списка lst на куски размером n
 -- (последний кусок может быть меньше)
 groups :: [a] -> Integer -> [[a]]
-groups lst n = todo
+groups list n = unfoldr (\x -> if null x then Nothing
+                                         else Just (mySplitAt n x)) list
