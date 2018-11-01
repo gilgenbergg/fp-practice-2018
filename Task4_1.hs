adfdaad32e817ff71110dbb50c5e50b49d2aff5e
gilgenbergg <lanasvega@gmail.com>
2018-11-02 00:53:58 +0300

    Task 4 is done

--- Task4_1.hs
+++ Task4_1.hs
@@ -7,33 +7,33 @@ data FunMonad a = FunMonad { fun :: String -> a }
 -- реализуйте классы `Functor`, `Applicative` и `Monad` для типа `FunMonad`
 
 instance Monad FunMonad where
-	--(>>=) :: FunMonad a -> (a -> FunMonad b) -> FunMonad b
-	(>>=) (FunMonad f1) f2 = FunMonad (\anything -> fun (f2 (f1 anything)) anything)
-	return a = FunMonad (\anything -> a)
+    --(>>=) :: FunMonad a -> (a -> FunMonad b) -> FunMonad b
+    (>>=) (FunMonad f1) f2 = FunMonad (\anything -> fun (f2 (f1 anything)) anything)
+    return a = FunMonad (\anything -> a)
 
 instance  Functor FunMonad where
-	{-fmap :: 
-	   (a -> b) 
-	   -> FunMonad a 
-	   -> FunMonad b
+    {-fmap :: 
+       (a -> b) 
+       -> FunMonad a 
+       -> FunMonad b
     -}
-	--fmap f (FunMonad a) = FunMonad (f . a)
-	fmap = liftM
+    --fmap f (FunMonad a) = FunMonad (f . a)
+    fmap = liftM
 
 instance Applicative FunMonad where
-	{-
-	(<*>) :: FunMonad (a -> b) -> FunMonad a -> FunMonad b
-	-}
-	--(<*>) fmab fma = FunMonad (\anything -> fun fmab anything (fun fma anything))
-	--[(+),(*)] <*> [1,2] <*> [3,4]
-	--[4,5,5,6,3,4,6,8]
+    {-
+    (<*>) :: FunMonad (a -> b) -> FunMonad a -> FunMonad b
+    -}
+    --(<*>) fmab fma = FunMonad (\anything -> fun fmab anything (fun fma anything))
+    --[(+),(*)] <*> [1,2] <*> [3,4]
+    --[4,5,5,6,3,4,6,8]
 
-	-- ((+) <*> (+5)) 1
-	--7
+    -- ((+) <*> (+5)) 1
+    --7
 
-	--(<*>) (FunMonad f1) (FunMonad f2) = FunMonad(\anything -> f1 anything (f2 anything))
-	pure = return
-	(<*>) = ap
-	
+    --(<*>) (FunMonad f1) (FunMonad f2) = FunMonad(\anything -> f1 anything (f2 anything))
+    pure = return
+    (<*>) = ap
+    
 
 
--- Task4_2.hs
+++ Task4_2.hs
@@ -1,7 +1,26 @@
 module Task4_2 where
+import Control.Monad
 
 data FourOf a = FourOf a a a a deriving(Show,Eq)
 
 -- реализуйте классы `Functor`, `Applicative` и `Monad` для типа `FourOf`
 -- таким образом, что 
 -- do { x <- FourOf 1 2 3 4; y <- FourOf 4 6 7 8; return $ x + y } === FourOf 5 8 10 12
+
+f1 (FourOf s _ _ _) = s
+f2 (FourOf _ s _ _) = s
+f3 (FourOf _ _ s _) = s
+f4 (FourOf _ _ _ s) = s
+
+instance Monad (FourOf) where
+	(>>=) (FourOf a b c d ) f = FourOf (f1 (f a)) (f2 (f b)) (f3 (f c)) (f4 (f d))
+	return s = FourOf s s s s
+
+instance Functor FourOf where
+	fmap = liftM
+	--fmap f (FourOf a b c d ) = FourOf (f a) (f b) (f c) (f d)
+
+instance Applicative FourOf where
+	(<*>) = ap 
+	--(<*>) (FourOf a b c d) (FourOf a' b' c' d') = FourOf (a a') (b b') (c c') (d d')
+	pure = return 
