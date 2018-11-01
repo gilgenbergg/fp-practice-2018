d5e333650281d2b590310ea563f04450c661501b
gilgenbergg <lanasvega@gmail.com>
2018-11-01 15:22:54 +0300

    Task4_1 is done

--- Task4_1.hs
+++ Task4_1.hs
@@ -1,31 +1,39 @@
 module Task4_1 where
 
+import Control.Monad
 -- Монада над функцией. В качестве входного значения `fun` может быть что угодно
 -- Собственно, почему бы не `String`?
 data FunMonad a = FunMonad { fun :: String -> a }
-
 -- реализуйте классы `Functor`, `Applicative` и `Monad` для типа `FunMonad`
+
+instance Monad FunMonad where
+	--(>>=) :: FunMonad a -> (a -> FunMonad b) -> FunMonad b
+	(>>=) (FunMonad f1) f2 = FunMonad (\anything -> fun (f2 (f1 anything)) anything)
+	return a = FunMonad (\anything -> a)
+
 instance  Functor FunMonad where
 	{-fmap :: 
 	   (a -> b) 
 	   -> FunMonad a 
 	   -> FunMonad b
     -}
-	fmap f (FunMonad a) = FunMonad (f . a)
+	--fmap f (FunMonad a) = FunMonad (f . a)
+	fmap = liftM
 
 instance Applicative FunMonad where
-	pure a = FunMonad (\anything -> a)
 	{-
 	(<*>) :: FunMonad (a -> b) -> FunMonad a -> FunMonad b
 	-}
-	(<*>) fmab fma = FunMonad (\anything -> fun fmab anything (fun fma anything))
+	--(<*>) fmab fma = FunMonad (\anything -> fun fmab anything (fun fma anything))
 	--[(+),(*)] <*> [1,2] <*> [3,4]
 	--[4,5,5,6,3,4,6,8]
 
 	-- ((+) <*> (+5)) 1
 	--7
 
---doesn`t work for now, in progress
---instance Monad FunMonad where
---	(>>=) monadA monadB = FunMonad (\anything -> fun (monadB (fun monadA anything) anything)
---	return a = pure a
+	--(<*>) (FunMonad f1) (FunMonad f2) = FunMonad(\anything -> f1 anything (f2 anything))
+	pure = return
+	(<*>) = ap
+	
+
+
