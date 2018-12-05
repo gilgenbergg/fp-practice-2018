8e569a4f7598fc1ff93cf093f24d93c2f15b3d8e
gilgenbergg <lanasvega@gmail.com>
2018-12-06 01:05:13 +0300

    Task_5 is done

--- Task5_1.hs
+++ Task5_1.hs
@@ -30,10 +30,34 @@ list2dlist' left (h: t) =
     in rec
 
 index :: DList a -> Int -> a
-index = todo
+index DNil _ = error "Emptyness inside the list :("
+index dlist idx = if idx < 0 then error "idx is not positive enough!"
+                              else baseIndex dlist idx where
+    baseIndex (DCons _ cur _) 0 = cur
+    baseIndex (DCons DNil _ DNil) idx | idx /= 0 = error "Out of bounds"
+    baseIndex (DCons _ _ r) ind = baseIndex r (ind - 1)
+
+
+flattenLeft :: DList a -> DList a
+flattenLeft DNil = DNil
+flattenLeft normalized@(DCons DNil _ _) = normalized
+flattenLeft (DCons (DCons ll lv lr) cur right) = DCons ll lv (DCons lr cur right)
+
 
 insertAt :: DList a -> Int -> a -> DList a
-insertAt list index value = todo
+insertAt list index value = if index < 0 then error "index is too negative!"
+                              else browseL (flattenLeft list) index value where
+browseL DNil i insertion | i == 0 = DCons DNil insertion DNil
+                         | otherwise = error "End of list"
+browseL (DCons DNil cur right) 0 insertion = DCons DNil insertion (DCons DNil cur right)
+browseL (DCons DNil cur right) index insertion = DCons DNil cur (browseL right (index-1) insertion)
+
 
 removeAt :: DList a -> Int -> DList a
-removeAt list index = todo
+removeAt list index = remover (flattenLeft list) index where
+  remover DNil _ = error "Emptyness inside the list :("
+  remover (DCons DNil cur DNil) index | index == 0 = DNil
+                                      | otherwise = error "No such index"
+  remover (DCons DNil cur (DCons DNil rc rr)) 0 = DCons DNil rc rr
+  remover (DCons DNil cur right) index = if (index < 0) then error "negative!" else 
+                      DCons DNil cur (remover right (index-1)) 
--- Task5_2.hs
+++ Task5_2.hs
@@ -2,11 +2,12 @@ module Task5_2 where
 
 import Todo(todo)
 import Data.Ratio
+import Prelude hiding (scanl, take, zipWith, (%))
 
 data Stream a = Cons {
                     shead :: a,
                     stail :: Stream a
-                }
+                } deriving Show
 
 srepeat :: a -> Stream a
 srepeat x =
@@ -31,8 +32,38 @@ instance Monad Stream where
     return x = srepeat x
     ss >>= f = sflatten (f <$> ss)
 
+
+generateS :: (Eq a, Num a) => a -> a -> Stream a
+generateS 1 0 = Cons (-1) (generateS 0 1)
+generateS (-1) 0 = Cons (1) (generateS 0 (-1))
+generateS 0 1 = Cons 0 (generateS (-1) 0)
+generateS 0 (-1) = Cons 0 (generateS 1 0)
+
+scanl :: (a -> a -> a) -> a -> Stream a -> Stream a
+scanl f init (Cons shead stail) = Cons init (scanl f (f init shead) stail)
+
+factorials :: Stream Integer
+factorials = scanl (*) 1 (generate 1 (+ 1))
+
+denoms :: Num a => a -> Stream a
+denoms x = fmap (x^) (generate 0 (+ 1))
+
+sin' :: Stream Double -> Stream Double
+sin' = zipWith (*) (generateS 0 1) 
+
+--основная функция комбинирования
 sinPrecisions :: Double -> Stream Double
-sinPrecisions = todo
+sinPrecisions x = stail (scanl (+) 0 (sin' (zipWith (/) (denoms x) (fmap fromInteger factorials))))
+
+exp' :: Rational -> Stream Rational
+exp' x = zipWith (/) (denoms x) (fmap toRational factorials)
 
 ePrecisions :: Stream Rational
-ePrecisions = todo
+ePrecisions = stail (scanl (+) 0 (exp' 1))
+
+zipWith :: (a -> a -> a) -> Stream a -> Stream a -> Stream a
+zipWith fun (Cons shead1 stail1) (Cons shead2 stail2) = Cons (fun shead1 shead2) (zipWith fun stail1 stail2)
+
+take :: Integer -> Stream a -> [a]
+take 0 stream = []
+take x (Cons shead stail) = shead : (take (x - 1) stail)
